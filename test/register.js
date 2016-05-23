cf54ba680dea377adcd77209d06fb0f37d812a66
Fran Varney <fvarney@progress.com>
2016-05-23 16:17:56 -0400

    Complete register test

--- test/register.js
+++ test/register.js
@@ -3,31 +3,35 @@
 const Code = require('code')
 const Hapi = require('hapi')
 const Lab = require('lab')
-const Sinon = require('sinon')
 
-const Decorate = require('../lib/decorate')
 const HapiTreeize = require('../lib/register')
 
 let lab = exports.lab = Lab.script()
-let before = lab.before
 let describe = lab.describe
 let it = lab.it
 let expect = Code.expect
 
-let server = Sinon.createStubInstance(Hapi.Server)
-
 describe('lib/register', () => {
   describe('when registering the plugin', () => {
-    before((done) => {
-      server.decorate.returns(Decorate.reply)
-      done()
-    })
-
     it('decorates reply', (done) => {
-      HapiTreeize.register(server, {}, (err) => {
+      let server = new Hapi.Server()
+      server.connection()
+
+      server.register(HapiTreeize, (err) => {
         expect(err).to.be.undefined()
-        expect(server.decorate.called).to.be.true()
-        expect(server.decorate.returned(Decorate.reply)).to.be.true()
+      })
+
+      server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, reply) => reply.treeize([[1, true]], null, ['id', 'value'])
+      })
+
+      server.inject({
+        method: 'GET',
+        url: '/'
+      }, (response) => {
+        expect(response.result).to.deep.equal([{ id: 1, value: true }])
         done()
       })
     })
